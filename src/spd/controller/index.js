7fce58e19a5a2ab3d42d601c2e7d2a79d355f4be
梁山 <liangshan@liangshandeMacBook-Pro.local>
2020-01-01 00:50:07 +0800

    no message

--- src/spd/controller/index.js
+++ src/spd/controller/index.js
@@ -38,7 +38,7 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 const qs = require('querystring');
 
-const puppeteer = require('puppeteer')
+// const puppeteer = require('puppeteer')
 
 module.exports = class extends enkel.controller.base {
   init (http) {
@@ -84,103 +84,103 @@ module.exports = class extends enkel.controller.base {
     })
   }
 
-  async sAction () {
-    const that = this
-    let params = this.get()
+  // async sAction () {
+  //   const that = this
+  //   let params = this.get()
 
-    let kw = params.kw ? decodeURIComponent(params.kw) : '找工作'
-    let maxPage = params.mp ? Number(params.mp) : 5
-    let target = params.target ? decodeURIComponent(params.target).split(';') : ['智联', '.zhaopin.com']
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto('https://www.baidu.com')
+  //   let kw = params.kw ? decodeURIComponent(params.kw) : '找工作'
+  //   let maxPage = params.mp ? Number(params.mp) : 5
+  //   let target = params.target ? decodeURIComponent(params.target).split(';') : ['智联', '.zhaopin.com']
+  //   const browser = await puppeteer.launch()
+  //   const page = await browser.newPage()
+  //   await page.goto('https://www.baidu.com')
 
-    await page.type('#kw', kw, { delay: 50 })
+  //   await page.type('#kw', kw, { delay: 50 })
 
-    await page.click('#su')
-    await page.waitForSelector('#container')
+  //   await page.click('#su')
+  //   await page.waitForSelector('#container')
 
-    function sleep (ms) {
-      return new Promise(resolve => setTimeout(resolve, ms))
-    }
-    async function findIndex (count, maxPages) {
-      let _maxPages = maxPages + 1
-      if (_maxPages > maxPage) {
-        return count
-      }
-      let outIndex = count
-      let _index = await page.$eval('#container', el => {
-        // let zp = ['前程无忧', '.51job.com']
-        let zp = ['百姓网', 'baixing.com']
-        // let zp = ['智联', '.zhaopin.com']
-        let results = Array.from($(el).find('.result'))
-        let _text = ''
-        let _i = -1
-        for (let i = 0; i < results.length; i++) {
-          _text = results[i].querySelector('.f13').innerText
-          if (zp.some(item => _text.indexOf(item) > -1)) {
-            _i = i
-            i = results.length
-          }
-        }
-        // return results[0].querySelector('.f13').innerText
-        return _i + 1
-      })
+  //   function sleep (ms) {
+  //     return new Promise(resolve => setTimeout(resolve, ms))
+  //   }
+  //   async function findIndex (count, maxPages) {
+  //     let _maxPages = maxPages + 1
+  //     if (_maxPages > maxPage) {
+  //       return count
+  //     }
+  //     let outIndex = count
+  //     let _index = await page.$eval('#container', el => {
+  //       // let zp = ['前程无忧', '.51job.com']
+  //       let zp = ['百姓网', 'baixing.com']
+  //       // let zp = ['智联', '.zhaopin.com']
+  //       let results = Array.from($(el).find('.result'))
+  //       let _text = ''
+  //       let _i = -1
+  //       for (let i = 0; i < results.length; i++) {
+  //         _text = results[i].querySelector('.f13').innerText
+  //         if (zp.some(item => _text.indexOf(item) > -1)) {
+  //           _i = i
+  //           i = results.length
+  //         }
+  //       }
+  //       // return results[0].querySelector('.f13').innerText
+  //       return _i + 1
+  //     })
 
-      // console.log('.....22......', _index)
-      // console.log('>>>>> count2: ', count, _index)
-      if (_index === 0) {
+  //     // console.log('.....22......', _index)
+  //     // console.log('>>>>> count2: ', count, _index)
+  //     if (_index === 0) {
 
-        let nextPage = 'https://www.baidu.com' + await page.$eval('#page  .n', el => el.getAttribute('href'))
-        await page.goto(nextPage)
-        await page.waitForSelector('#container')
-        count += await page.$eval('#container', el => Array.from($(el).find('.result')).length)
-        let _newIndex = await findIndex(count, _maxPages)
-        outIndex = _newIndex
-      } else {
-        outIndex += _index
-      }
-      console.log('.... index: ', outIndex, 'page: ', _maxPages)
-      return outIndex
-    }
+  //       let nextPage = 'https://www.baidu.com' + await page.$eval('#page  .n', el => el.getAttribute('href'))
+  //       await page.goto(nextPage)
+  //       await page.waitForSelector('#container')
+  //       count += await page.$eval('#container', el => Array.from($(el).find('.result')).length)
+  //       let _newIndex = await findIndex(count, _maxPages)
+  //       outIndex = _newIndex
+  //     } else {
+  //       outIndex += _index
+  //     }
+  //     console.log('.... index: ', outIndex, 'page: ', _maxPages)
+  //     return outIndex
+  //   }
 
-    let _index = findIndex(0, 0)
+  //   let _index = findIndex(0, 0)
 
-    // let outIndex = -1
-    // let _text = ''
-    // for (let i = 0; i < _results.length; i++) {
-    //   _text = _results[i].querySelector('.f13').innerText
-    //   if (zp.some(item => _text.indexOf(item) > -1)) {
-    //     outIndex = i
-    //     i = _results.length
-    //   }
-    // }
-    // if (outIndex = -1) {
-    //   $(el).find('#page .n')
-    // }
-    // await browser.close()
-    return this.json(({
-      status: 200,
-      message: '搜索成功',
-      data: {
-        ranking: _index
-      }
-    }))
-    let requestUrl = `https://www.baidu.com/s?wd=%E6%89%BE%E5%B7%A5%E4%BD%9C&rsv_spt=1&rsv_iqid=0xc6dbedae00094cb6&issp=1&f=8&rsv_bp=1&rsv_idx=2&ie=utf-8&rqlang=cn&tn=baiduhome_pg&rsv_enter=1&oq=puppeteer&rsv_t=dd4fbijeDkUamxPGdTX1Gt2lGR1r5HbfzorKp0RiXuLkUPorDOMK33Y3l3GvEO4py03s&inputT=3284&rsv_pq=ebeebcab000c88f5&rsv_sug3=35&rsv_sug1=33&rsv_sug7=100&bs=puppeteer`
-    if (params.ip) {
-      requestUrl += '?ip=' + params.ip
-    }
-    return axios.get(requestUrl).catch(err => {
-      return this.json({ status: 1002, message: '查询失败，请稍后再试', data: {} })
-    }).then(({ data }) => {
-      const $ = cheerio.load(data)
-      return this.json({
-        status: 200, message: '成功', data: {
-          ip: $('#result').find('code').eq(0).text(),
-          address: $('#result').find('code').eq(1).text(),
-          geoIp: $('#result').html().replace(/(.*geoip:)([^<]*)(<\/p>.*)/i, '$2').trim()
-        }
-      })
-    })
-  }
+  //   // let outIndex = -1
+  //   // let _text = ''
+  //   // for (let i = 0; i < _results.length; i++) {
+  //   //   _text = _results[i].querySelector('.f13').innerText
+  //   //   if (zp.some(item => _text.indexOf(item) > -1)) {
+  //   //     outIndex = i
+  //   //     i = _results.length
+  //   //   }
+  //   // }
+  //   // if (outIndex = -1) {
+  //   //   $(el).find('#page .n')
+  //   // }
+  //   // await browser.close()
+  //   return this.json(({
+  //     status: 200,
+  //     message: '搜索成功',
+  //     data: {
+  //       ranking: _index
+  //     }
+  //   }))
+  //   let requestUrl = `https://www.baidu.com/s?wd=%E6%89%BE%E5%B7%A5%E4%BD%9C&rsv_spt=1&rsv_iqid=0xc6dbedae00094cb6&issp=1&f=8&rsv_bp=1&rsv_idx=2&ie=utf-8&rqlang=cn&tn=baiduhome_pg&rsv_enter=1&oq=puppeteer&rsv_t=dd4fbijeDkUamxPGdTX1Gt2lGR1r5HbfzorKp0RiXuLkUPorDOMK33Y3l3GvEO4py03s&inputT=3284&rsv_pq=ebeebcab000c88f5&rsv_sug3=35&rsv_sug1=33&rsv_sug7=100&bs=puppeteer`
+  //   if (params.ip) {
+  //     requestUrl += '?ip=' + params.ip
+  //   }
+  //   return axios.get(requestUrl).catch(err => {
+  //     return this.json({ status: 1002, message: '查询失败，请稍后再试', data: {} })
+  //   }).then(({ data }) => {
+  //     const $ = cheerio.load(data)
+  //     return this.json({
+  //       status: 200, message: '成功', data: {
+  //         ip: $('#result').find('code').eq(0).text(),
+  //         address: $('#result').find('code').eq(1).text(),
+  //         geoIp: $('#result').html().replace(/(.*geoip:)([^<]*)(<\/p>.*)/i, '$2').trim()
+  //       }
+  //     })
+  //   })
+  // }
 }
