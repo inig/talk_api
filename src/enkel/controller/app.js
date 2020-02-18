f0b0930bda68782186d73b1d5ec7e44d7da0f8cc
lsliangshan <loveliangshan@126.com>
2020-02-18 23:32:22 +0800

    no message

---
+++ src/enkel/controller/app.js
@@ -0,0 +1,83 @@
+const axios = require('axios');
+const qs = require('querystring');
+
+module.exports = class extends enkel.controller.base {
+  init (http) {
+    super.init(http);
+
+    this.Op = this.Sequelize.Op;
+
+    this.XINLI_KEY = '046b6a2a43dc6ff6e770255f57328f89'
+
+    this.response.setHeader('Access-Control-Allow-Origin', '*');
+    this.response.setHeader('Access-Control-Allow-Headers', '*');
+    this.response.setHeader('Access-Control-Allow-Methods', '*');
+  }
+
+  async getRadioAction () {
+    let params = this.get()
+
+    let now = new Date()
+    let date = now.getFullYear() + '-' + (now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1) + '-' + (now.getDate() < 10 ? '0' + now.getDate() : now.getDate())
+    let ts = now.getTime()
+    await axios.get(`http://tacc.radio.cn/pcpages/radiopages?callback=jQuery11220497578513847472_${ts}&place_id=${params.city || 3225}&date=${date}&_=${ts}`).catch(err => {
+      return this.json({ status: 1002, message: err.message || '获取失败，请稍后再试', data: {} })
+    }).then(response => {
+      let data = {}
+      data = response.data.replace(new RegExp('^jQuery11220497578513847472_' + ts + '\\\('), '').replace(/\)$/, '')
+      try {
+        data = JSON.parse(data)
+      } catch (err) {
+        data = {}
+      }
+      return this.json({
+        status: 200,
+        message: '成功',
+        data: data.data
+      })
+    })
+  }
+
+  async getFmHomeListAction () {
+    await axios.get(`http://yiapi.xinli001.com/fm/home-list.json?key=${this.XINLI_KEY}`).catch(err => {
+      return this.json({ status: 1002, message: err.message || '获取失败，请稍后再试', data: {} })
+    }).then(data => {
+      return this.json({
+        status: 200,
+        message: '成功',
+        data: data.data.data || []
+      })
+    })
+  }
+
+  async getFmNewListAction () {
+    let params = this.get()
+    let offset = ((params.pageIndex - 1) || 0) * (params.pageSize || 20)
+    let limit = params.pageSize || 20
+    await axios.get(`http://yiapi.xinli001.com/fm/newfm-list.json?offset=${offset}&limit=${limit}&key=${this.XINLI_KEY}`).catch(err => {
+      return this.json({ status: 1002, message: err.message || '获取失败，请稍后再试', data: {} })
+    }).then(data => {
+      return this.json({
+        status: 200,
+        message: '成功',
+        data: data.data.data || []
+      })
+    })
+  }
+
+  async getFmAnchroCategoryListDetailAction () {
+    let params = this.get()
+    let offset = ((params.pageIndex - 1) || 0) * (params.pageSize || 20)
+    let limit = params.pageSize || 20
+    await axios.get(`http://yiapi.xinli001.com/fm/category-jiemu-list.json?category_id=${params.id}&offset=${offset}&limit=${limit}&key=${this.XINLI_KEY}`).catch(err => {
+      return this.json({ status: 1002, message: err.message || '获取失败，请稍后再试', data: {} })
+    }).then(data => {
+      return this.json({
+        status: 200,
+        message: '成功',
+        data: data.data.data || []
+      })
+    })
+  }
+
+}
